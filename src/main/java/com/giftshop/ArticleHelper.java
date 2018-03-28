e420112bbfb65104d5129440032365b8891577a5
OGHN <guillermo.hdz.navarro@gmail.com>
2018-03-28 02:04:59 -0700

    Helper class for Article Object

---
+++ src/main/java/com/giftshop/ArticleHelper.java
@@ -0,0 +1,13 @@
+package com.giftshop;
+
+/**
+ * Created by O HERNANDEZ
+ * on 27/03/2018.
+ */
+
+public class ArticleHelper {
+
+    // defined querys
+    public static final String QUERY_ALL_ARTICLES = "from Article";
+
+}
